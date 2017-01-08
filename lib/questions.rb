aaab997e0b1d31484fcbb3557a37f09e354c774b
Mike Hurl <mikehurl@gmail.com>
2017-01-08 13:29:27 +0000

    test to return domain name from email address passes

--- lib/questions.rb
+++ lib/questions.rb
@@ -179,6 +179,7 @@ end
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  email.split("@")[1].split(".")[0]
 end
 
 # capitalize the first letter in each word of a string,
