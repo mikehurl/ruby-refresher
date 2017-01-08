997c5b430da34ddcb02e186bc8c6f299fb5a295b
Mike Hurl <mikehurl@gmail.com>
2017-01-08 13:56:31 +0000

    test to titleize a string passes

--- lib/questions.rb
+++ lib/questions.rb
@@ -188,6 +188,11 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  exceptions = ['a', 'and', 'the']
+  words = string.split
+  words.first.capitalize!
+  words.each { |word| exceptions.include?(word) ? word : word.capitalize! }
+  words.join(" ")
 end
 
 # return true if a string contains any special characters
