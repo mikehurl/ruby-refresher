43ba019995381514165f1909136394e68a14c32e
Mike Hurl <mikehurl@gmail.com>
2017-01-08 14:57:02 +0000

    test to check if a given date is a UK bank holiday in 2014 passes

--- lib/questions.rb
+++ lib/questions.rb
@@ -240,6 +240,8 @@ end
 # the list of bank holidays is here:
 # https://www.gov.uk/bank-holidays
 def is_a_2014_bank_holiday?(date)
+  bank_holidays = ["01/01", "18/04", "21/04", "05/05", "26/05", "25/08", "25/12", "26/12"]
+  bank_holidays.include?(date.strftime("%d/%m"))
 end
 
 # given your birthday this year, this method tells you
